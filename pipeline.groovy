72849df74c9df6e4500c240e608f963fcd8b2459
Adil Labiad <adillabiad@gmail.com>
2025-02-22 19:56:30 -0500

    add pipeline script

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/adilblanco/jenkins-flask-app.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/adilblanco/jenkins-hello-world.git']]])
             }
         }
         stage('Install Dependencies') {
